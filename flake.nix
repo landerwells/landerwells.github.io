2096595eb7e8eeb47a6f5f3120a3453f521cca2e
landerwells <landerwells@gmail.com>
2026-03-08 18:31:19 -0500

    Adding hugo so I don't have to have a system dependency

--- flake.nix
+++ flake.nix
@@ -1,15 +1,21 @@
 {
-  description = "A very basic flake";
+  description = "A Hugo site built with Nix";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            hugo
+          ];
+        };
+      });
 }
